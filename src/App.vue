3da6a5941033479e0674437a08ee0f0274b5c612
Tom Chan <>
2025-07-10 00:12:19 +0930

    feat: add dark mode to app

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,11 @@
 <template>
-  <h1>Taskey</h1>
+  <div :class="{ dark: mode == 'dark' }">
+    <h1>Taskey</h1>
+  </div>
 </template>
+
+<script setup lang="ts">
+import { useColorMode } from '@vueuse/core'
+
+const mode = useColorMode()
+</script>
