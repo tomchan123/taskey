611ea53f2b96db5cc92c0e2dc4652d94559ee179
Tom Chan <>
2025-07-15 00:05:45 +0930

    feat: added card component

--- src/views/SetupView.vue
+++ src/views/SetupView.vue
@@ -1,5 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import TaskeyCard from '@/components/ui/TaskeyCard.vue'
+</script>
 
 <template>
   <h1>This is the setup page</h1>
+  <TaskeyCard>
+    <p>This is the setup page content</p>
+  </TaskeyCard>
 </template>
