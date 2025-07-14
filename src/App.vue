db385a7438194082e7d1606b5a34c3362cc59a32
Tom Chan <>
2025-07-14 23:17:50 +0930

    feat: added initial background colors

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,7 @@ const mode = useColorMode()
 </script>
 
 <template>
-  <div :class="{ dark: mode == 'dark' }">
-    <h1>Taskey Title</h1>
+  <div :class="{ dark: mode === 'dark' }" class="bg-background text-foreground h-dvh">
     <main>
       <RouterView />
     </main>
