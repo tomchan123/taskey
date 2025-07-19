6ae25babc8c6bd22d96ae9a04e9650582b5ad857
Tom Chan <>
2025-07-20 01:08:33 +0930

    feat: added collapsible card component

---
+++ src/components/ui/CollapsibleCard/CollapsibleCard.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { CollapsibleRoot } from 'reka-ui'
+
+interface IProps {
+  disabled?: boolean
+  open: boolean
+}
+const { open, disabled = false } = defineProps<IProps>()
+</script>
+
+<template>
+  <CollapsibleRoot
+    :open="open"
+    :disabled="disabled"
+    class="bg-card text-card-foreground rounded-2xl p-4"
+  >
+    <slot />
+  </CollapsibleRoot>
+</template>
---
+++ src/components/ui/CollapsibleCard/CollapsibleCardContent.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { CollapsibleContent } from 'reka-ui'
+</script>
+
+<template>
+  <CollapsibleContent
+    class="data-[state=open]:animate-collapsible-slide-down data-[state=closed]:animate-collapsible-slide-up"
+  >
+    <slot />
+  </CollapsibleContent>
+</template>
+
+<style lang="css"></style>
